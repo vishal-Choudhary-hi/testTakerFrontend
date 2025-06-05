72932ffdbf82eb40bb4ae88bb094e912f075ee06
vishal-Choudhary-hi <vishalchoudhary34510@gmail.com>
2025-06-05 22:49:52 +0530

    api base url change

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:4000";
+const baseUrl = "https://testtakerbackend.onrender.com/";
 
 const apiCall = async (method, endpoint, data = {}, showSnackbar, authRequired = true) => {
     try {
